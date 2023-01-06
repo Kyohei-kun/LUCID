2e1e4ff73da11a548ebcefd73fe9892996db360e
Kyohei-kun <cedric.gazzano@gmail.com>
2023-01-06 16:49:38 +0100

    Link animation elevator

---
+++ Assets/_DEVELOPPER/GENERAL_SUCRE/Scripts/Batiments/Elevator/CS_Elevator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Elevator : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            animator.SetBool("PlayerIsIn", true);
+            StopAllCoroutines();
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Wait(1));
+        }
+    }
+
+    public IEnumerator Wait(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        animator.SetBool("PlayerIsIn", false);
+    }
+}
