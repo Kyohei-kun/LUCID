719be5f5006f95122e3c6b8713b8b6b30b937a1a
Kyohei-kun <cedric.gazzano@gmail.com>
2023-01-12 10:17:12 +0100

    Link animation and material Transfer Building

--- Assets/_DEVELOPPER/GENERAL_SUCRE/Scripts/Building/Transfert/CS_Transfert.cs
+++ Assets/_DEVELOPPER/GENERAL_SUCRE/Scripts/Building/Transfert/CS_Transfert.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class CS_Transfert : CS_ElectricBuilding
 {
+    [SerializeField] float EU_By_Second;
+
+    [Header("Animations")]
+    [Space(10)]
+    [SerializeField] Animator animator;
+
+    [Header("Materials Feedbacks")]
+    [Space(10)]
+    [SerializeField] Renderer visuelRender;
+    [SerializeField] Material sourceMaterial;
+
     private CS_Battery receiverBattery;
     Coroutine co_Transfert;
-    [SerializeField] float EU_By_Second;
+
+    private void Start()
+    {
+        visuelRender.materials[1] = new Material(sourceMaterial);
+    }
 
     public void SetBattery(CS_Battery bat, bool isReceiver)
     {
@@ -37,18 +52,25 @@ public class CS_Transfert : CS_ElectricBuilding
     public new void StartWork()
     {
         co_Transfert = StartCoroutine(TransfertEnergy());
+        animator.SetBool("IfOn", true);
+        visuelRender.materials[1].SetFloat("_OnOff", 1);
     }
 
     public new void StopWork()
     {
         if(co_Transfert != null)
         StopCoroutine(co_Transfert);
+        animator.SetBool("IfOn", false);
+        visuelRender.materials[1].SetFloat("_OnOff", 0);
     }
 
     IEnumerator TransfertEnergy()
     {
         while (true)
         {
+            if (battery.CurrentEnergy == 0)
+                StopWork();
+
             float valueTransfert = Time.deltaTime.Remap(0, 1, 0, EU_By_Second);
             if(battery.CurrentEnergy >= valueTransfert)
             battery.Charge(-receiverBattery.Charge(valueTransfert));
