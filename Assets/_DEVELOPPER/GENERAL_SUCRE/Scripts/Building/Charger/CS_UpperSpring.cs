b0d848acf3d8c986f6bbbef5a6773ba16c9e4cce
Kyohei-kun <cedric.gazzano@gmail.com>
2023-01-25 16:08:40 +0100

    Relink visual manual charger

--- Assets/_DEVELOPPER/GENERAL_SUCRE/Scripts/Building/Charger/CS_UpperSpring.cs
+++ Assets/_DEVELOPPER/GENERAL_SUCRE/Scripts/Building/Charger/CS_UpperSpring.cs
@@ -7,7 +7,9 @@ public class CS_UpperSpring : MonoBehaviour
     Rigidbody _rigidbody;
     [SerializeField] float playerMass;
     [SerializeField] Transform underSpring;
+    [SerializeField] Transform middleBone;
     [SerializeField] CS_Charger charger;
+    [SerializeField] float distanceThresholdEnergy;
     bool canCharge = true;
 
     private void Start()
@@ -15,12 +17,17 @@ public class CS_UpperSpring : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        middleBone.position = new Vector3(middleBone.position.x, underSpring.position.y + (Vector3.Distance(underSpring.position, gameObject.transform.position) /2), middleBone.position.z);
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
             _rigidbody.AddForce(Vector3.down * Time.deltaTime * playerMass);
-            if (Vector3.Distance(gameObject.transform.position, underSpring.position) < 0.5f)
+            if (Vector3.Distance(gameObject.transform.position, underSpring.position) < distanceThresholdEnergy)
             {
                 if (canCharge)
                 {
