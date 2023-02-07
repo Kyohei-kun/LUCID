08007863e405dd206b48468bc980bcecb9354808
Kyohei-kun <cedric.gazzano@gmail.com>
2023-02-07 16:14:46 +0100

    Add visuel scrap and lingot

--- Assets/_DEVELOPPER/GENERAL_SUCRE/Scripts/Items/CS_Scrap.cs
+++ Assets/_DEVELOPPER/GENERAL_SUCRE/Scripts/Items/CS_Scrap.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CS_Scrap : CS_Item
@@ -8,5 +9,18 @@ public class CS_Scrap : CS_Item
     {
         base.Start();
         enumItem = E_Item.Scrap;
+
+        List<Transform> childs = GetComponentsInChildren<Transform>().ToList();
+
+        foreach (var item in childs)
+        {
+            item.gameObject.SetActive(false);
+        }
+        int index = Random.Range(1, childs.Count);
+
+
+        childs[index].gameObject.SetActive(true);
+        childs[0].gameObject.SetActive(true);
+
     }
 }
